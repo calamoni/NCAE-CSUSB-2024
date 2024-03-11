40e5bccd0b9924bac7c22e43f9287a22a89553f5
skitpack <95957245+skitpack@users.noreply.github.com>
2024-03-11 15:59:06 -0700

    Add files via upload
    Enumeration tool

---
+++ prostagma.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Search for all users available on the system and output to a file
+#
+echo -e "\033[1mAll available users\033[0m"
+#
+cat /etc/passwd | cut -d ':' -f 1 
+#
+# Search for all users with a shell
+#
+echo -e "\033[1mAll available users with a shell\033[0m" 
+
+cat /etc/passwd | grep -e /bin/bash -e /bin/zsh -e /bin/sh | cut -d ':' -f 1 
+
+echo -e "\033[1mAll network services that are listening and running\033[0m"
+
+netstat -lp
+#
