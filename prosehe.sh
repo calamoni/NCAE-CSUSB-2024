1578e9e4f313312a4e3a8fce1b039b4f8f78711b
skitpack <95957245+skitpack@users.noreply.github.com>
2024-03-12 22:45:55 -0700

    Add files via upload
    Automation log script

---
+++ prosehe.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+LOG_FILE="/var/log/system_network.log"
+
+echo "Enter the log that you wish."
+echo "1. tcp/udp"
+echo "2. system"
+echo "3. etc"
+
+echo -n "Log: "
+
+read log 
+
+if [ $log = '1' ]; then 
+
+collect_tcp_udp_info() { 
+	echo "=============== Network Information ==============="
+	echo "Date: $(date)"
+	echo "---------------------------------------------------"
+	ifconfig -a 
+	echo "---------------- ALL TCP PORTS --------------------"
+	ss -t 
+	echo "---------------- ALL UDP PORTS---------------------"
+	ss -u  
+	echo "------------- ALL LISTENING PORTS -----------------"
+	ss -l | grep -e tcp -e udp
+	echo "---------------------------------------------------"
+	echo
+}
+
+while true; do 
+	{ collect_tcp_udp_info; }
+	sleep 5 
+
+done
+
+elif [ $log = '2' ];  then
+
+
+collect_system_info() {
+	echo "=============== System Information ==============="
+	echo "Date: $(date)" 
+	echo "--------------------------------------------------"
+	uname -a
+	echo "--------------------------------------------------"
+	df -h 
+	echo "--------------------------------------------------"
+	free -h 
+	echo "--------------------------------------------------"
+	uptime 
+	echo "--------------------------------------------------"
+	echo
+
+}
+
+while true; do
+	{ collect_system_info; } 
+		sleep 5
+
+	done
+
+elif [ $log = '3' ]; then
+
+	collect_etc_info() {	
+	echo "=============== ETC ==============="
+	echo "Date: $(date)"
+	echo "-----------------------------------"
+	ss -aux 
+	echo "-----------------------------------"
+	uptime 
+	echo "-----------------------------------"
+	echo 
+}
+while true; do
+	{ collect_etc_info; }
+		sleep 5 
+
+	done 
+fi
+
